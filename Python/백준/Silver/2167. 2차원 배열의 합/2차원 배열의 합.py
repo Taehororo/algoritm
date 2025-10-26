75b3b75ad46293af934f3ac0e71c3ececd85726a
전태호 <145996139+Taehororo@users.noreply.github.com>
2025-10-26 22:08:13 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 112 ms, Memory: 38948 KB -BaekjoonHub

--- Python/백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ Python/백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,15 +1,26 @@
 import sys
-sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
-N, M = map(int, input().split())
-arr = [[0]*(M+1)] + [[0] + list(map(int, input().split())) for _ in range(N)]
-K = int(input())
-arr2 = [list(map(int, input().split())) for _ in range(K)]
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
-ans = []
+# 누적합 테이블 초기화 (1-index로 사용)
+prefix = [[0] * (m+1) for _ in range(n+1)]
 
-for i,j,x,y in arr2:
-  cnt = 0
-  for t in range(i,x+1):
-    cnt += sum(arr[t][j:y+1])
-  print(cnt)
+# 누적합 계산
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        prefix[i][j] = (arr[i-1][j-1]
+                        + prefix[i-1][j]
+                        + prefix[i][j-1]
+                        - prefix[i-1][j-1])
+
+# 쿼리 처리
+k = int(input())
+for _ in range(k):
+    i, j, x, y = map(int, input().split())
+    result = (prefix[x][y] 
+              - prefix[i-1][y] 
+              - prefix[x][j-1] 
+              + prefix[i-1][j-1])
+    print(result)
