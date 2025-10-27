ae86ef53d6e4c7dfee33190f763fecf0c64022d4
전태호 <145996139+Taehororo@users.noreply.github.com>
2025-10-28 00:13:45 +0900

    [Gold IV] Title: 연구소, Time: 2104 ms, Memory: 35016 KB -BaekjoonHub

--- Python/백준/Gold/14502. 연구소/연구소.py
+++ Python/백준/Gold/14502. 연구소/연구소.py
@@ -1,43 +1,54 @@
-from itertools import combinations
+import sys
 from collections import deque
+from itertools import combinations
 import copy
+sys.setrecursionlimit(10**6)
 
-# 입력
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
 lab = [list(map(int, input().split())) for _ in range(N)]
 
-# 빈칸 위치 찾기
-empty = [(i, j) for i in range(N) for j in range(M) if lab[i][j] == 0]
 
-# BFS로 바이러스 확산
-def spread_virus(board):
-    q = deque()
-    for i in range(N):
-        for j in range(M):
-            if board[i][j] == 2:
-                q.append((i, j))
-    
-    while q:
-        x, y = q.popleft()
-        for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0:
-                board[nx][ny] = 2
-                q.append((nx, ny))
-    return board
+empty = []
+for i in range(N):
+  for j in range(M):
+    if lab[i][j] == 0:
+      empty.append((i,j))
+      
+def bfs(board):
+  
+  q = deque([])
+  for i in range(N):
+    for j in range(M):
+      if board[i][j] == 2:
+        q.append((i,j))
+  
+  while q:
+    x, y = q.popleft()
+    for dx, dy in ((-1,0),(1,0),(0,1),(0,-1)):
+      nx = x+dx
+      ny = y+dy
+      if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0:
+        board[nx][ny] = 2
+        q.append((nx,ny))
+  return board
 
-# 안전 영역 계산
+
 def safe_area(board):
-    return sum(row.count(0) for row in board)
+  cnt = 0
+  for i in range(N):
+    for j in range(M):
+      if board[i][j] == 0:
+        cnt += 1
+  return cnt
 
-max_safe = 0
+max_cnt = 0
 
-# 모든 벽 조합 시뮬레이션
-for walls in combinations(empty, 3):
-    temp_lab = copy.deepcopy(lab)
-    for x, y in walls:
-        temp_lab[x][y] = 1
-    temp_lab = spread_virus(temp_lab)
-    max_safe = max(max_safe, safe_area(temp_lab))
-
-print(max_safe)
+for walls in combinations(empty,3):
+  temp_lab = copy.deepcopy(lab)
+  for x,y in walls:
+    temp_lab[x][y] = 1
+  temp_lab = bfs(temp_lab)
+  max_cnt = max(max_cnt,safe_area(temp_lab))
+print(max_cnt)
