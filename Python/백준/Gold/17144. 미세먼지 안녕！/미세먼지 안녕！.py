2da17f130f6854fb98ccf8fd4a9718bff718c31f
전태호 <145996139+Taehororo@users.noreply.github.com>
2025-10-29 13:59:31 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 2796 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ Python/백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,126 +1,108 @@
 import sys
-import copy
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-R, C, T = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(R)]
+#미세먼지 확산시키는 함수
+def spread():  
+    v=[[0]*C for _ in range(R)]
+    
+    #4방향 검사 후 v배열에 값들을 저장해둔다.
+    for i in range(R):
+       for j in range(C):
+            if arr[i][j] != 0 and arr[i][j] != -1:
+                for k in range(4):
+                    nx = i+dx[k]
+                    ny = j+dy[k]
+                    
+                    if 0<=nx<R and 0<=ny<C and arr[nx][ny] != -1:
+                        mungi = arr[i][j] // 5
+                        v[nx][ny] += mungi
+                        v[i][j] -= mungi
+    
+    #위의 검사가 끝나고난 후 arr에 v배열 값들을 합침
+    for i in range(R):
+        for j in range(C):
+            arr[i][j]+=v[i][j]
 
-# 청정기 위치
-clener = []
-for i in range(R):
-  if board[i][0] == -1:
-    clener.append((i,0))
-  
+#공기청정기 위에부분 청소
+def clean_up():
+    #동쪽부터시작하니 d=0
+    d=0
+    before=0
+    #로봇의 시작부분 다음칸부터 시작
+    x, y = machine[0][0],machine[0][1]+1
+    
+    #로봇이 공기청정기를 만나거나 벽을만나면 방향을 꺾는 조건문 추가
+    while True:
+        nx = x+dx[d]
+        ny = y+dy[d]
+        
+        if nx == R or ny == C or nx == -1 or ny == -1:
+            d = (d-1)%4
+            continue
+        
+        if x == machine[0][0] and y == machine[0][1]:
+            break
+        
+        arr[x][y], before = before, arr[x][y]
+        x,y = nx,ny
 
+#공기청정기 아래부분 청소
+def clean_down():
+    #동쪽부터시작하니 d=0
+    d=0
+    before=0
+    
+    #로봇의 시작부분 다음칸부터 시작
+    x,y = machine[1][0],machine[1][1]+1
+    
+    #로봇이 공기청정기를 만나거나 벽을만나면 방향을 꺾는 조건문 추가
+    while True:
+        nx = x+dx[d]
+        ny = y+dy[d]
+        
+        if nx == R or ny == C or nx == -1 or ny == -1:
+            d = (d+1)%4
+            continue
+            
+        if x == machine[1][0] and y ==machine[1][1]:
+            break
+        
+        arr[x][y],before = before,arr[x][y]
+        x,y=nx,ny
 
-def dust(air):
-  temp_air = copy.deepcopy(air)
-  for i in range(R):
-    for j in range(C):
-      # 확산이 일어나는 조건
-      if air[i][j] >= 5:
-        x, y = i, j
-        # 4방향중 확산된 수
-        cnt = 0
-        for dx, dy in ((-1,0),(1,0),(0,1),(0,-1)):
-          nx = x + dx
-          ny = y + dy
-          if 0 <= nx < R and 0 <= ny < C and air[nx][ny] != -1:
-            cnt += 1
-            temp_air[nx][ny] += air[x][y] // 5
-        temp_air[x][y] -= (air[x][y] // 5)*cnt
-  return temp_air
+if __name__=='__main__':
+    R,C,T = map(int,input().split())
+    arr = [list(map(int, input().split())) for _ in range(R)]
+    
+    #로봇의 위치 저장하는 배열
+    machine=[]
+    
+    #미세먼지 누적 합 저장
+    result=0
+    
+    #동남서북 4방향
+    dx = [0,1,0,-1]
+    dy = [1,0,-1,0]
 
-def clean_1(air):
-  temp_air = copy.deepcopy(air)
-  x1, y1 = clener[0]
-  # 청정기 바로 옆부터 시작해야 해서
-  y1 += 1
-  temp_air[x1][y1] = 0
-  while True:
-    ny = y1 + 1
-    if 0 <= ny < C:
-      temp_air[x1][ny] = air[x1][y1]
-      y1 = ny
-    else:
-      break
-  while True:
-    nx = x1 - 1
-    if 0 <= nx < R:
-      temp_air[nx][y1] = air[x1][y1]
-
-      x1 = nx
-    else:
-      break
-  while True:
-    ny = y1 -1
-    if 0 <= ny < C:
-      temp_air[x1][ny] = air[x1][y1]
-
-      y1 = ny
-    else:
-      break
-  while True:
-    nx = x1 + 1
-    if 0 <= nx < R and air[nx][y1] != -1:
-      temp_air[nx][y1] = air[x1][y1]
-
-      x1 = nx
-    else:
-      break
-  return temp_air
-
-def clean_2(air):
-  temp_air = copy.deepcopy(air)
-  x2, y2 = clener[1]
-  # 청정기 바로 옆부터 시작해야 해서
-  y2 += 1
-  temp_air[x2][y2] = 0
-  while True:
-    ny = y2 + 1
-    if 0 <= ny < C:
-      temp_air[x2][ny] = air[x2][y2]
-      y2 = ny
-    else:
-      break
-  while True:
-    nx = x2 + 1
-    if 0 <= nx < R:
-      temp_air[nx][y2] = air[x2][y2]
-
-      x2 = nx
-    else:
-      break
-  while True:
-    ny = y2 -1
-    if 0 <= ny < C:
-      temp_air[x2][ny] = air[x2][y2]
-
-      y2 = ny
-    else:
-      break
-  while True:
-    nx = x2 - 1
-    if 0 <= nx < R  and air[nx][y2] != -1:
-      temp_air[nx][y2] = air[x2][y2]
-
-      x2 = nx
-    else:
-      break
-  return temp_air 
-
-
-def cnt_dust(air):
-  cnt = 0
-  for i in range(R):
-    for j in range(C):
-      if air[i][j] > 0:
-        cnt += air[i][j]
-  return cnt
-
-for _ in range(T):
-  board = dust(board)
-  board = clean_1(board)
-  board = clean_2(board)
-print(cnt_dust(board))
+    
+    #로봇의 위치찾아서 machine 배열에 저장
+    for i in range(R):
+        for j in range(C):
+            if arr[i][j] == -1:
+                machine.append((i,j))
+                
+    
+    #입력받은 T만큼 반복
+    for _ in range(T):
+        spread()
+        clean_up()
+        clean_down()
+    
+    #미세먼지의 양을 찾아서 result에 누적함
+    for i in range(R):
+        for j in range(C):
+            if arr[i][j] != 0 and arr[i][j] != -1:
+                result += arr[i][j]
+    
+    print(result)
