0da458cef3bdb527edec320eb7d5014bb11eec3a
전태호 <145996139+Taehororo@users.noreply.github.com>
2025-10-31 21:04:46 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34944 KB -BaekjoonHub

--- Python/백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ Python/백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,41 +1,27 @@
-# 2178 미로탐색
 import sys
 from collections import deque
-from pprint import pprint 
-# input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
-# def dfs(x,y,depth):
-#     global answer
-#     if x == N-1 and y == M-1:
-#         answer = min(answer,depth)
-#         return
-    
-#     for dx, dy in ((-1,0),(1,0),(0,1),(0,-1)):
-#         nx = x + dx
-#         ny = y + dy
-#         if 0 <= nx < N and 0 <= ny < M:
-#             if visited[nx][ny] == 0 and arr[nx][ny] =='1':
-#                 visited[nx][ny] = 1
-#                 dfs(nx,ny,depth+1)
-#                 visited[nx][ny] = 0
-
-# dfs는 시간초과가 자꾸나!!!!!!!
-
-def bfs(x,y):
-    q = deque([(x,y)])
-    while q:
-        x,y = q.popleft()
-        for dx, dy in ((-1,0),(1,0),(0,-1),(0,1)): 
-            nx = x + dx
-            ny = y + dy
-            if 0 <= nx < N and 0 <= ny < M:
-                if arr[nx][ny] == 1:
-                    arr[nx][ny] = arr[x][y] + 1
-                    q.append((nx,ny))
-  
-                
+input = sys.stdin.readline
 N, M = map(int, input().split())
-arr = [list(map(int, input())) for _ in range(N)]
-bfs(0,0)
-print(arr[N-1][M-1])
+arr = [input().rstrip() for _ in range(N)]
+
+
+def bfs(board):
+  visited = [[-1]*M for _ in range(N)]
+  visited[0][0] = 1
+  q = deque([(0,0)])
+  
+  while q:
+    x, y = q.popleft()
+    if x == N-1 and y == M-1:
+      break
+    for dx, dy in ((-1,0),(1,0),(0,1),(0,-1)):
+      nx = x + dx
+      ny = y + dy
+      if 0 <= nx < N and 0 <= ny < M and visited[nx][ny] == -1 and board[nx][ny] =='1':
+        q.append((nx,ny))
+        visited[nx][ny] = visited[x][y] + 1
+  return visited[N-1][M-1]
+
+print(bfs(arr))
